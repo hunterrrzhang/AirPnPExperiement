070e1ae29a7d1d91d24f9807fe417afa5688692b
HunterZhang1017 <73006539+hunterrrzhang@users.noreply.github.com>
2021-11-19 14:32:07 -0600

    -

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ cors = CORS(app)
 
 @cross_origin()
 
-@app.route('/api/', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def main():
     
     if flask.request.method == 'GET':
